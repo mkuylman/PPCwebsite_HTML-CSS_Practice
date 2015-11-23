b9895f4d21a802f94ed06bbf3e289328050cefca
mkuylman <mkuylman@gmail.com>
2015-11-22 17:06:53 -0800

    PPC webpage coded from the ground up using HTML/CSS

---
+++ introI.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+</head>
+
+<body>
+
+	<div ="script">		
+		<?php
+		for($i = 1; $i <= 1000; $i++2) {
+				echo $i;
+		}
+		?>
+	</div>
+	
+</body>
+</html>
